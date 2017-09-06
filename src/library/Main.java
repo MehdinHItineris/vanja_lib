051941a26ccc47a059b3d2adce8d9dbd5e192ec0
Vanja Mihajlovic <31698194+vanjamih@users.noreply.github.com>
2017-09-06 16:20:38 +0200

    test

--- src/library/Main.java
+++ src/library/Main.java
@@ -5,7 +5,7 @@ import library.ui.FMain;
 public class Main {
     public static void main(String[] args) {
         
-        // Git test
+        // Git test, dodatak
         FMain fMain = new FMain();
         fMain.setVisible(true);        
     }    
