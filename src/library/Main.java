b187b69ae4dbda973041a061ee13b579c854203c
Mehdin Hodzic <mehdinh@itineris.ba>
2017-09-06 16:02:44 +0200

    Test git

--- src/library/Main.java
+++ src/library/Main.java
@@ -4,6 +4,8 @@ import library.ui.FMain;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // Git test
         FMain fMain = new FMain();
         fMain.setVisible(true);        
     }    
